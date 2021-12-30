21ebcfce989423dae76d9afc2ce935dada81202b
seroroman <47804854+seroroman@users.noreply.github.com>
2021-12-30 16:56:44 +0900

    Inlinecss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import { InlineStyles } from "./components/InlineStyles";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <InlineStyles />
     </div>
   );
 }
